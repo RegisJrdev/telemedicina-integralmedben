81edf316a557332cfb5746ffc176207b470212db
RegisJrdev <regisjrdev@gmail.com>
2026-02-18 11:44:06 -0300

    Wip

--- config/tenancy.php
+++ config/tenancy.php
@@ -17,10 +17,10 @@ return [
      * Only relevant if you're using the domain or subdomain identification middleware.
      */
     'central_domains' => [
+        'telemedicinamedben.com.br',
         '127.0.0.1',
         'localhost',
         'localhost:8000',
-        'telemedicinamedben.com.br',
     ],
 
     /**
